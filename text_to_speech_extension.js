740045f2288c4530c7489498e5cf985e3274b5c2
Benjamin Berg <Mallory@Benjamins-MacBook-Pro.local>
2015-07-19 17:07:24 -0400

    attempting language support
       -testing with EN, ES, FR first
       -placeholder function to translate not yet functional

--- text_to_speech_extension.js
+++ text_to_speech_extension.js
@@ -1,51 +1,79 @@
 /* Extension using the JavaScript Speech API for text to speech */
 /* Sayamindu Dasgupta <sayamindu@media.mit.edu>, April 2014 */
 
-new (function() {
-    var ext = this;
+new(function() {
+   var ext = this;
 
-    /*function _get_voices() {
-        var ret = [];
-        var voices = speechSynthesis.getVoices();
-        
-        for(var i = 0; i < voices.length; i++ ) {
-            ret.push(voices[i].name);
-            console.log(voices.toString());
-        }
+   /*function _get_voices() {
+       var ret = [];
+       var voices = speechSynthesis.getVoices();
+       
+       for(var i = 0; i < voices.length; i++ ) {
+           ret.push(voices[i].name);
+           console.log(voices.toString());
+       }
 
-        return ret;
-    }
+       return ret;
+   }
 
-    ext.set_voice = function() {
-    };*/
+   ext.set_voice = function() {
+   };*/
 
-    ext.speak_text = function (text, callback) {
-        var u = new SpeechSynthesisUtterance(text.toString());
-        u.onend = function(event) {
-            if (typeof callback=="function") callback();
-        };
-        
-        speechSynthesis.speak(u);
-    };
+   ext.speak_text = function(text, callback) {
+      var u = new SpeechSynthesisUtterance(text.toString());
+      u.onend = function(event) {
+         if (typeof callback == "function") callback();
+      };
 
-    ext._shutdown = function() {};
+      speechSynthesis.speak(u);
+   };
 
-    ext._getStatus = function() {
-        if (window.SpeechSynthesisUtterance === undefined) {
-            return {status: 1, msg: 'Your browser does not support text to speech. Try using Google Chrome or Safari.'};
-        }
-        return {status: 2, msg: 'Ready'};
-    };
+   ext.speak_text_in = function(text, lang, callback) {
+      var u = new SpeechSynthesisUtterance(text.toString());
+      u.lang = lang;
+      u.onend = function(event) {
+         if (typeof callback == "function") callback();
+      };
 
-    var descriptor = {
-        blocks: [
-            //['', 'set voice to %m.voices', 'set_voice', ''],
-            ['w', 'speak %s', 'speak_text', 'Hello!'],
-        ],
-        /*menus: {
-            voices: _get_voices(),
-        },*/
-    };
+      speechSynthesis.speak(u);
+   };
 
-    ScratchExtensions.register('Text to Speech', descriptor, ext);
+   ext.translate_and_speak = function(text, lang, callback) {
+      var u = new SpeechSynthesisUtterance(text.toString());
+      u.lang = lang;
+      u.onend = function(event) {
+         if (typeof callback == "function") callback();
+      };
+
+      speechSynthesis.speak(u);
+   };
+
+   ext._shutdown = function() {};
+
+   ext._getStatus = function() {
+      if (window.SpeechSynthesisUtterance === undefined) {
+         return {
+            status: 1,
+            msg: 'Your browser does not support text to speech. Try using Google Chrome or Safari.'
+         };
+      }
+      return {
+         status: 2,
+         msg: 'Ready'
+      };
+   };
+
+   var descriptor = {
+      blocks: [
+         //['', 'set voice to %m.voices', 'set_voice', ''],
+         ['w', 'speak %s', 'speak_text', 'Hello!'],
+         ['w', 'say %s in %m.lang', 'speak_text_in', 'Hello!'],
+         ['w', 'translate %s from %m.lang to %m.lang', 'translate_and_speak', 'Hello!']
+      ],
+      menus: {
+         lang: ['en-US', 'es-ES', 'fr-FR']
+      }
+   };
+
+   ScratchExtensions.register('Text to Speech', descriptor, ext);
 })();
